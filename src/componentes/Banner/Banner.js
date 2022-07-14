6f430e573140e3a6ba00fa1ea5e7e8c368728e03
Sara <saraeliza.lirio@gmail.com>
2022-07-14 10:15:03 -0300

    Explorando o projeto

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -2,7 +2,9 @@ import './Banner.css'
 
 function Banner() {
     return (
-        <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+        </header>
     )
 }
 
