0adbe6f9764e7ba72e84514f92aff5af9ac78a62
Sara <saraeliza.lirio@gmail.com>
2022-07-15 17:18:38 -0300

    Criando o componente formulário

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
+import Formulario from './componentes/Formulario';
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto';
+
 
 function App() {
   return (
     <div className="App">
       <Banner />       
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
